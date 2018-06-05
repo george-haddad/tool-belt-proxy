7faa0d1f6408842d3dad5085db2b28163ab5869d
George Haddad <george-haddad@users.noreply.github.com>
2018-06-05 16:06:27 +0300

    [wip] Feat spacex routes (#8)
    * chore: simplify require statements
    
    * chore: make code style pretty
    
    * feat: add spacex routes, decouple version validation

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const dotenv = require('dotenv');
 const express = require('express');
-const validate = require('express-validation');
 
-const versionValidation = require('./lib/validation/version');
 const defaultRouter = require('./lib/routers/default');
 const errorHandler = require('./lib/errors/errorHandler');
 const ErrStrategies = require('./lib/errors/strategies');
 const indexRoute = require('./lib/routes/index');
 const readyRoute = require('./lib/routes/ready').ready;
 const axfrRoute = require('./lib/routes/axfr');
+const spacexRoute = require('./lib/routes/spacex');
 const logger = require('./lib/utils/logger').Logger;
 
 dotenv.config({ silent: true });
@@ -17,7 +16,6 @@ const app = express();
 const appErrorHandler = errorHandler([ErrStrategies.defaultStrategy]);
 const PORT = process.env.PORT || 5000;
 
-app.use(/^\/(?!ready).*/, validate(versionValidation.checkVersion));
 app.use((req, res, next) => {
   req.getVersion = function validateVer() {
     return req.headers.accept.split('version=')[1];
@@ -28,6 +26,7 @@ app.use((req, res, next) => {
 app.use('/', indexRoute(defaultRouter()));
 app.use('/ready', readyRoute(defaultRouter()));
 app.use('/axfr', axfrRoute(defaultRouter()));
+app.use('/spacex', spacexRoute(defaultRouter()));
 
 appErrorHandler(app);
 
